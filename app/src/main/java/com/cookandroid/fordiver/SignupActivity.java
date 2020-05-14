175549b33e37fb6bf32ba7d76e10bd396cd3247d
최은정 <morned270@daum.net>
2020-05-14 11:45:28 +0900

    added initial, login, signup activitiy and each xml file

---
+++ app/src/main/java/com/cookandroid/fordiver/SignupActivity.java
@@ -0,0 +1,21 @@
+package com.cookandroid.fordiver;
+package com.example.jo.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
