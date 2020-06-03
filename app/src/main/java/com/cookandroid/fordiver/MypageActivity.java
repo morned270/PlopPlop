26e5595baf73067e639b13ad503739b111946b0a
jyHyun1014 <zex199@naver.com>
2020-06-03 21:06:16 +0900

    updated LoginActivity, MypageActivity, activity_mypage

--- app/src/main/java/com/cookandroid/fordiver/MypageActivity.java
+++ app/src/main/java/com/cookandroid/fordiver/MypageActivity.java
@@ -7,22 +7,22 @@ import android.widget.TextView;
 
 public class MypageActivity extends AppCompatActivity {
 
-    private TextView tv_id, tv_pass;
+    private TextView tv_name, tv_course;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
+        tv_name = findViewById(R.id.tv_name);
+        tv_course = findViewById(R.id.tv_course);
 
         Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
+        String userName = intent.getStringExtra("userName");
+        String userCourse = intent.getStringExtra("userCourse");
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
+        tv_name.setText(userName);
+        tv_course.setText(userCourse);
 
     }
 }
