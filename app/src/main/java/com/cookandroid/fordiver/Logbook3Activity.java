13d98c268bf3ceeb9eb49d158dc6f6aae4dde92d
LEMONSHARK <key0416sh@naver.com>
2020-06-03 23:53:12 +0900

    add switch logbook2 and logbook3

--- app/src/main/java/com/cookandroid/fordiver/Logbook3Activity.java
+++ app/src/main/java/com/cookandroid/fordiver/Logbook3Activity.java
@@ -1,13 +1,36 @@
 package com.cookandroid.fordiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Logbook3Activity extends AppCompatActivity {
 
+    Switch switchlogview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logbook3);
+
+        switchlogview = (Switch) findViewById(R.id.switchlogview);
+
+        switchlogview.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true){
+                    //Toast.makeText(Logbook3Activity.this, "스위치-ON", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Logbook3Activity.this, Logbook2Activity.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                } else {
+                    //Toast.makeText(Logbook3Activity.this, "스위치-OFF", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 }
