8f5b891e5696d9ef5d24bb7c4af9261578eea7d6
최은정 <morned270@daum.net>
2020-06-10 23:51:52 +0900

    added Quiz1activity

---
+++ app/src/main/java/com/cookandroid/fordiver/Quiz1Activity.java
@@ -0,0 +1,13 @@
+package com.cookandroid.fordiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quiz1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz1);
+    }
+}
