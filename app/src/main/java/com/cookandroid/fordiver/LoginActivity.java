32aea46ac5f6acefb33ff81e146f0f88b31cb1f9
jyHyun1014 <zex199@naver.com>
2020-06-10 16:34:00 +0900

    Merge branch 'master' of https://github.com/morned270/PlopPlop
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/res/layout/activity_initial.xml

--- app/src/main/java/com/cookandroid/fordiver/LoginActivity.java
+++ app/src/main/java/com/cookandroid/fordiver/LoginActivity.java
@@ -8,11 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
