ae94b23d9d021428b5a1c41b657e32c2bdb888e0
jyHyun1014 <zex199@naver.com>
2020-06-10 23:54:58 +0900

    pull

--- app/src/main/java/com/cookandroid/fordiver/CommunityActivity.java
+++ app/src/main/java/com/cookandroid/fordiver/CommunityActivity.java
@@ -1,29 +1,25 @@
 package com.cookandroid.fordiver;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-public class MainActivity extends Fragment{
-    private View view;
+public class CommunityActivity extends Fragment {
 
-    public MainActivity() {
+    public CommunityActivity() {
 
     }
+
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_main, container, false);
+        view = inflater.inflate(R.layout.activity_community, container, false);
 
         return view;
     }
--- app/src/main/java/com/cookandroid/fordiver/MypageActivity.java
+++ app/src/main/java/com/cookandroid/fordiver/MypageActivity.java
@@ -1,28 +1,41 @@
 package com.cookandroid.fordiver;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MypageActivity extends AppCompatActivity {
+public class MypageActivity extends Fragment {
 
+    private View view;
     private TextView tv_name, tv_course;
 
+    public MypageActivity() {
+
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_mypage, container, false);
 
-        tv_name = findViewById(R.id.tv_name);
-        tv_course = findViewById(R.id.tv_course);
+        tv_name = view.findViewById(R.id.tv_name);
+        tv_course = view.findViewById(R.id.tv_course);
 
-        Intent intent = getIntent();
+        Intent intent = getActivity().getIntent();
         String userName = intent.getStringExtra("userName");
         String userCourse = intent.getStringExtra("userCourse");
 
         tv_name.setText(userName);
         tv_course.setText(userCourse);
 
+        return view;
     }
 }
