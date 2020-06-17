60c17344475e00d71023c9b123cccd847078d075
LEMONSHARK <key0416sh@naver.com>
2020-06-18 00:41:15 +0900

    to pull quiz4page(0618)

---
+++ app/src/main/java/com/cookandroid/fordiver/Quiz2Activity.java
@@ -0,0 +1,44 @@
+package com.cookandroid.fordiver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Quiz2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz2);
+
+        Button btn_ow = (Button)findViewById(R.id.btn_q_ow);
+        Button btn_aow = (Button)findViewById(R.id.btn_q_aow);
+
+        btn_ow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //값 넘겨주기
+                //액티비티 전환 Intent 정의
+                Intent intent = new Intent(Quiz2Activity.this, Quiz3Activity.class);
+                intent.putExtra("course","OpenWater"); /*송신*/
+
+                startActivity(intent);
+            }
+        });
+
+        btn_aow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //값 넘겨주기
+                //액티비티 전환 Intent 정의
+                Intent intent = new Intent(Quiz2Activity.this, Quiz3Activity.class);
+                intent.putExtra("course","Advanced OpenWater");
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/cookandroid/fordiver/Quiz3Activity.java
+++ app/src/main/java/com/cookandroid/fordiver/Quiz3Activity.java
@@ -2,12 +2,160 @@ package com.cookandroid.fordiver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Quiz3Activity extends AppCompatActivity {
+
+    int quiz_num=0;
+    int correct_count=0;
+    int wrong_count=0;
+    int[] allresult=new int[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz3);
+        final Intent intent2quiz4 = new Intent(Quiz3Activity.this, Quiz2Activity.class);
+
+        TextView getcourse = (TextView)findViewById(R.id.quiz_course); //퀴즈 난이도 쓸 textview 선언
+
+        final TextView quiz_text = (TextView)findViewById(R.id.quiz_text);    //문제와 문제의 답 버튼들 선언
+        final Button btn_ans_1 = (Button)findViewById(R.id.btn_ans_1);
+        final Button btn_ans_2 = (Button)findViewById(R.id.btn_ans_2);
+        final Button btn_ans_3 = (Button)findViewById(R.id.btn_ans_3);
+        final Button btn_ans_4 = (Button)findViewById(R.id.btn_ans_4);
+
+        Intent intent = getIntent(); /*Quiz2 화면으로부터 데이터 전달받기위함*/
+        String course = intent.getExtras().getString("course"); /*선택한 퀴즈 단계 불러오기*/
+        getcourse.setText(course);
+
+        final String[] arr_text = getResources().getStringArray(R.array.quiz_text); /*quiz_aow 불러오기*/
+        final String[] arr_ans1 = getResources().getStringArray(R.array.ans_1);
+        final String[] arr_ans2 = getResources().getStringArray(R.array.ans_2);
+        final String[] arr_ans3 = getResources().getStringArray(R.array.ans_3);
+        final String[] arr_ans4 = getResources().getStringArray(R.array.ans_4);
+        final int[] arr_anslist = getResources().getIntArray(R.array.anslist);
+
+        quiz_text.setText(arr_text[0]); //첫번째 문제 띄우기
+        btn_ans_1.setText(arr_ans1[0]);
+        btn_ans_2.setText(arr_ans2[0]);
+        btn_ans_3.setText(arr_ans3[0]);
+        btn_ans_4.setText(arr_ans4[0]);
+
+        btn_ans_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int result = arr_anslist[quiz_num]==1?1:2; //정답이면 1 틀리면 2를
+                if(result==1)
+                    correct_count++;
+                else
+                    wrong_count++;
+
+                allresult[quiz_num]=result; // 결과리스트 allresult에 저장
+
+                if(quiz_num<9) {
+                    quiz_num++;// 다음문제로 넘어가기
+                    quiz_text.setText(arr_text[quiz_num]); //다음 문제 띄우기
+                    btn_ans_1.setText(arr_ans1[quiz_num]);
+                    btn_ans_2.setText(arr_ans2[quiz_num]);
+                    btn_ans_3.setText(arr_ans3[quiz_num]);
+                    btn_ans_4.setText(arr_ans4[quiz_num]);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "맞은 문제는 "+correct_count+"개, 틀린문제는 "+wrong_count, Toast.LENGTH_SHORT).show();
+                    startActivity(intent2quiz4);
+                //결과 화면으로 넘어가기
+                }
+            }
+        });
+
+        btn_ans_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int result = arr_anslist[quiz_num]==2?1:2; //정답이면 1 틀리면 2를
+                if(result==1)
+                    correct_count++;
+                else
+                    wrong_count++;
+
+                allresult[quiz_num]=result; // 결과리스트 allresult에 저장
+
+                if(quiz_num<9) {
+                    quiz_num++;// 다음문제로 넘어가기
+                    quiz_text.setText(arr_text[quiz_num]); //다음 문제 띄우기
+                    btn_ans_1.setText(arr_ans1[quiz_num]);
+                    btn_ans_2.setText(arr_ans2[quiz_num]);
+                    btn_ans_3.setText(arr_ans3[quiz_num]);
+                    btn_ans_4.setText(arr_ans4[quiz_num]);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "맞은 문제는 "+correct_count+"개, 틀린문제는 "+wrong_count, Toast.LENGTH_SHORT).show();
+                    startActivity(intent2quiz4);
+                    //결과 화면으로 넘어가기
+                }
+            }
+        });
+
+        btn_ans_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int result = arr_anslist[quiz_num]==3?1:2; //정답이면 1 틀리면 2를
+                if(result==1)
+                    correct_count++;
+                else
+                    wrong_count++;
+
+                allresult[quiz_num]=result; // 결과리스트 allresult에 저장
+
+                if(quiz_num<9) {
+                    quiz_num++;// 다음문제로 넘어가기
+                    quiz_text.setText(arr_text[quiz_num]); //다음 문제 띄우기
+                    btn_ans_1.setText(arr_ans1[quiz_num]);
+                    btn_ans_2.setText(arr_ans2[quiz_num]);
+                    btn_ans_3.setText(arr_ans3[quiz_num]);
+                    btn_ans_4.setText(arr_ans4[quiz_num]);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "맞은 문제는 "+correct_count+"개, 틀린문제는 "+wrong_count, Toast.LENGTH_SHORT).show();
+                    startActivity(intent2quiz4);
+                    //결과 화면으로 넘어가기
+                }
+            }
+        });
+
+        btn_ans_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int result = arr_anslist[quiz_num]==4?1:2; //정답이면 1 틀리면 2를
+                if(result==1)
+                    correct_count++;
+                else
+                    wrong_count++;
+
+                allresult[quiz_num]=result; // 결과리스트 allresult에 저장
+
+                if(quiz_num<9) {
+                    quiz_num++;// 다음문제로 넘어가기
+                    quiz_text.setText(arr_text[quiz_num]); //다음 문제 띄우기
+                    btn_ans_1.setText(arr_ans1[quiz_num]);
+                    btn_ans_2.setText(arr_ans2[quiz_num]);
+                    btn_ans_3.setText(arr_ans3[quiz_num]);
+                    btn_ans_4.setText(arr_ans4[quiz_num]);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "맞은 문제는 "+correct_count+"개, 틀린문제는 "+wrong_count, Toast.LENGTH_SHORT).show();
+                    startActivity(intent2quiz4);
+                    //결과 화면으로 넘어가기
+                }
+            }
+        });
     }
 }
