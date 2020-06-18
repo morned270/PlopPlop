48ecceec0be9c6a8f8ac35de6c92f18efd899b6e
Kimaram <laba98@naver.com>
2020-06-18 09:07:57 +0900

    지도 commit

--- app/src/main/java/com/cookandroid/fordiver/Logbook3Activity.java
+++ app/src/main/java/com/cookandroid/fordiver/Logbook3Activity.java
@@ -1,19 +1,35 @@
 package com.cookandroid.fordiver;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-public class Logbook3Activity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class Logbook3Activity extends FragmentActivity implements OnMapReadyCallback {
 
     Switch switchlogview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logbook3);
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
 
         switchlogview = (Switch) findViewById(R.id.switchlogview);
 
@@ -31,6 +47,16 @@ public class Logbook3Activity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        // ...
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(36.763695, 127.281796));
+        marker.setMap(naverMap);
 
     }
 }
